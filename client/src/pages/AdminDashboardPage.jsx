05ef2c0eaa5b258b7c1b1a7626a2e62c896e3f11
HandyWork1 <khalilbrainiarc1@gmail.com>
2024-03-18 10:01:21 +0300

    Set up Admin Dashboard page

--- client/src/pages/AdminDashboardPage.jsx
+++ client/src/pages/AdminDashboardPage.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { useAuth } from '../components/auth/AuthContext';
+import AdminSidebar from '../components/admin/AdminSideBar';
+import AdminNavbar from '../components/admin/AdminNavbar';
 
 const AdminDashboardPage = () => {
     const authContext = useAuth();
     const { isAuthenticated } = authContext;
     if (!isAuthenticated) return (<div>You are not logged in</div>);
     return(
-        <>
-            <h1>Admin Dashboard</h1>
-        </>
+        <div className="flex">
+            <AdminSidebar />
+            <div className="flex-1">
+                <AdminNavbar />
+                <h1 className="text-2xl font-bold">Admin Dashboard</h1>
+
+            </div>
+        </div>
     );
 }
 export default AdminDashboardPage;
