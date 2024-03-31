f89e39cf867742afe16a32d626a56c5eac582b29
HandyWork1 <khalilbrainiarc1@gmail.com>
2024-03-31 15:07:09 +0300

    Add user controller to fetch all users

---
+++ server/controllers/userController.js
@@ -0,0 +1,14 @@
+const { User } = require('../models/User');
+
+// Controller function to fetch all users
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+module.exports = { getUsers };
