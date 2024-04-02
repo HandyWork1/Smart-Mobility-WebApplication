375fe94fbc17eced07addf6bbd81e744c1511be3
HandyWork1 <khalilbrainiarc1@gmail.com>
2024-04-02 13:51:03 +0300

    Add theme management

--- client/src/pages/AdminDashboardPage.jsx
+++ client/src/pages/AdminDashboardPage.jsx
@@ -1,21 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Routes, Route, useNavigate } from "react-router-dom";
+import {toast} from 'react-toastify';
 import { useAuth } from '../components/auth/AuthContext';
 import AdminSidebar from '../components/admin/AdminSideBar';
 import AdminNavbar from '../components/admin/AdminNavbar';
+import Dashboard from '../components/admin/dashboard';
+import Users from '../components/admin/users';
+import ChallengesTipsPage from '../components/admin/content-management/ChallengesTipsPage';
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "../theme";
 
 const AdminDashboardPage = () => {
     const authContext = useAuth();
     const { isAuthenticated } = authContext;
-    if (!isAuthenticated) return (<div>You are not logged in</div>);
-    return(
-        <div className="flex">
-            <AdminSidebar />
-            <div className="flex-1">
-                <AdminNavbar />
-                <h1 className="text-2xl font-bold">Admin Dashboard</h1>
+    const [theme, colorMode] = useMode();
+    const [isSidebar, setIsSidebar] = useState(true);
+    const navigate = useNavigate();
 
+    // Alert Display for success 
+  const handleIntruderAlert = (message) => {
+    toast.error(message, {
+      position: 'top-center',
+      autoClose: 5000, 
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+
+    if (!isAuthenticated) {
+        handleIntruderAlert('User not logged in. Log in first');
+        navigate('/login');
+        return (
+            <div>
+                You are not logged in. Redirecting to login page...
             </div>
-        </div>
+        );
+    }
+
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <div className="admin-dashboard">
+                    <AdminSidebar isSidebar={isSidebar}/>
+                    <div className="main-content">
+                        <AdminNavbar setIsSidebar={setIsSidebar}/>
+                        <Routes>
+                            <Route path="/" element={<Dashboard />} />
+                            <Route exact path="/manage-users" element={<Users />} />
+                            <Route exact path="/manage-content" element={<ChallengesTipsPage />} />
+                        </Routes>
+                    </div>
+                </div>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
     );
-}
+};
+
 export default AdminDashboardPage;
