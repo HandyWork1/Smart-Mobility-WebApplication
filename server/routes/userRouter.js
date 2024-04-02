1b7ad1f87696258b87a7265453f8aff2f4ddacae
HandyWork1 <khalilbrainiarc1@gmail.com>
2024-04-02 13:57:38 +0300

    Add delete and add user endpoints to the router

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getUsers } = require('../controllers/userController');
+const userController = require('../controllers/userController');
 
 // Route to fetch all users
-router.get('/users', getUsers);
+router.get('/users', userController.getUsers);
+//  Route to delete user
+router.delete('/users/:id', userController.deleteUser);
+// Add a new user
+router.post('users', userController.addUser);
 
 module.exports = router;
