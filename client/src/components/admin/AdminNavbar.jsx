434794e243c514cbf8f5c3e927159963371f0fc6
HandyWork1 <khalilbrainiarc1@gmail.com>
2024-04-05 18:25:27 +0300

    Add new route for admin account

--- client/src/components/admin/AdminNavbar.jsx
+++ client/src/components/admin/AdminNavbar.jsx
@@ -56,9 +56,15 @@ const AdminNavbar = ({ toggleSidebar }) => {
         },
     }));
 
+    // Handle Redirect to AccountPage
+    const handleAccountRedirect = () =>{
+        handlePersonMenuClose();
+        navigate('/admin-dashboard/admin-account')
+    }
+
     // Logout Handling
     const handleLogout = () => {
-        // Logout logic, calling the logout function from your context
+        // Logout logic, calling the logout function from context
         logout();
         setLogoutModalOpen(false);
 
@@ -201,8 +207,8 @@ const AdminNavbar = ({ toggleSidebar }) => {
                     transformOrigin={{ horizontal: 'right', vertical: 'top' }}
                     anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
                 >
-                    <StyledMenuItem onClick={handlePersonMenuClose}>
-                    <Avatar /> My account
+                    <StyledMenuItem onClick={handleAccountRedirect}>
+                    <Avatar src={avatarPlaceholder} /> My account
                     </StyledMenuItem>
                     <Divider />
                     <StyledMenuItem onClick={handlePersonMenuClose}>
@@ -228,122 +234,6 @@ const AdminNavbar = ({ toggleSidebar }) => {
                 onLogout={handleLogout}
             />
         </Box>
-        // <div className="container bg-gray-800">
-        //     <div className="flex justify-between items-center px-12 py-2">
-        //         <div className="flex items-center">
-        //             {/* <div className="">
-        //                 <button onClick={toggleSidebar} className="text-white focus:outline-none">
-        //                     <i className="fas fa-bars fa-lg"></i>
-        //                 </button>
-        //             </div> */}
-        //             <div className="relative mr-4">
-        //                 <input type="text" placeholder="Search" className="bg-gray-700 text-white rounded-md px-3 py-1 focus:outline-none" />
-        //                 <i className="fas fa-search text-white absolute top-2 right-3"></i>
-        //             </div>
-        //         </div>
-        //         <div className="flex items-center">  
-        //             <div className="relative mr-4" ref={notificationsRef}>
-        //                 <i className="fas fa-bell text-white transition duration-300 ease-in-out" onClick={toggleNotificationsMenu}></i>
-        //                 {/* Notifications dropdown menu */}
-        //                 {isNotificationsOpen && (
-        //                     <div className="absolute top-full right-0 mt-7 w-72 bg-white rounded-md shadow-md overflow-hidden" ref={notificationsRef}>
-        //                         <ul className="py-2">
-        //                             <li className="px-4 py-3 flex items-start hover:bg-gray-100 cursor-pointer transition duration-300 ease-in-out">
-        //                                 <div className="bg-blue-500 text-white rounded-full w-10 h-10 flex items-center justify-center mr-3">
-        //                                     <i className="fas fa-envelope"></i>
-        //                                 </div>
-        //                                 <div>
-        //                                     <p className="text-sm font-semibold">New Message</p>
-        //                                     <p className="text-xs text-gray-600">You have received a new message from a user.</p>
-        //                                 </div>
-        //                             </li>
-        //                             <li className="px-4 py-3 flex items-start hover:bg-gray-100 cursor-pointer transition duration-300 ease-in-out">
-        //                                 <div className="bg-green-500 text-white rounded-full w-10 h-10 flex items-center justify-center mr-3">
-        //                                     <i className="fas fa-tasks"></i>
-        //                                 </div>
-        //                                 <div>
-        //                                     <p className="text-sm font-semibold">Task Completed</p>
-        //                                     <p className="text-xs text-gray-600">User successfully completed a task.</p>
-        //                                 </div>
-        //                             </li>
-        //                             <li className="px-4 py-3 flex items-start hover:bg-gray-100 cursor-pointer transition duration-300 ease-in-out">
-        //                                 <div className="bg-yellow-500 text-white rounded-full w-10 h-10 flex items-center justify-center mr-3">
-        //                                     <i className="fas fa-exclamation-triangle"></i>
-        //                                 </div>
-        //                                 <div>
-        //                                     <p className="text-sm font-semibold">Warning</p>
-        //                                     <p className="text-xs text-gray-600">Low disk space detected.</p>
-        //                                 </div>
-        //                             </li>
-        //                             <li className="px-4 py-3 flex items-start hover:bg-gray-100 cursor-pointer transition duration-300 ease-in-out">
-        //                                 <div className="bg-red-500 text-white rounded-full w-10 h-10 flex items-center justify-center mr-3">
-        //                                     <i className="fas fa-times"></i>
-        //                                 </div>
-        //                                 <div>
-        //                                     <p className="text-sm font-semibold">Error</p>
-        //                                     <p className="text-xs text-gray-600">Connection lost. Please check your network.</p>
-        //                                 </div>
-        //                             </li>
-        //                             <hr className="border-gray-300 my-2" />
-        //                             <li className="px-4 py-2 text-center text-blue-500 cursor-pointer hover:underline transition duration-300 ease-in-out">
-        //                                 View All
-        //                             </li>
-        //                         </ul>
-        //                     </div>
-        //                 )}
-        //             </div>
-
-
-        //             <div className="relative mr-4 flex items-center cursor-pointer" ref={profileRef}>
-        //                 <div
-        //                     className={`flex items-center justify-center w-full h-full px-5 py-2 rounded-2xl bg-gray-700 transition duration-300 ease-in-out transform ${isProfileHover ? 'hover:bg-gray-500' : ''}`}
-        //                     onClick={toggleProfileMenu}
-        //                     onMouseEnter={() => handleProfileHover(true)}
-        //                     onMouseLeave={() => handleProfileHover(true)}
-        //                 >
-        //                     <img
-        //                         src={userDetails.avatar || avatarPlaceholder}
-        //                         alt="User Avatar"
-        //                         className="w-10 h-10 rounded-full cursor-pointer"
-        //                     />
-        //                     <div
-        //                         className="ml-2 text-white text-md font-semibold cursor-pointer"
-        //                     >
-        //                         {userDetails.fullName}
-        //                     </div>
-        //                 </div>
-        //                 {/* Profile dropdown menu */}
-        //                 {isProfileOpen && (
-        //                     <div className="absolute top-full right-0 mt-2 w-48 bg-white rounded-md shadow-md z-10" ref={profileRef}>
-        //                         <ul className="py-2">
-        //                             <li className="px-4 py-2 hover:bg-gray-200 cursor-pointer transition duration-300 ease-in-out">
-        //                                 <i className="fas fa-user mr-2"></i>    
-        //                                 Profile
-        //                             </li>
-        //                             <li className="px-4 py-2 hover:bg-gray-200 cursor-pointer transition duration-300 ease-in-out">
-        //                                 <i className="fas fa-cog mr-2"></i>    
-        //                                 Settings
-        //                             </li>
-        //                             <li className="px-4 py-2 hover:bg-gray-200 cursor-pointer transition duration-300 ease-in-out"
-        //                                 onClick={() => {
-        //                                     setLogoutModalOpen(true);
-        //                                   }}
-        //                             >
-        //                                 <i className="fas fa-sign-out-alt mr-2"></i>
-        //                                 Logout
-        //                             </li>
-        //                         </ul>
-        //                     </div>
-        //                 )}
-        //             </div>
-        //         </div>
-        //     </div>
-        //     <LogoutModal
-        //         isOpen={isLogoutModalOpen}
-        //         onRequestClose={() => setLogoutModalOpen(false)}
-        //         onLogout={handleLogout}
-        //     />
-        // </div>
     );
 }
 
